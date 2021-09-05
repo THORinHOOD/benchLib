3899df94386c09a49095b0aa10b55c838dc4ff40
Asgar Zagitov <ihateyouthor@gmail.com>
2021-09-05 21:21:18 +0300

    0.0.31

--- src/main/java/com/db/benchLib/configs/BenchLibClientsConfig.java
+++ src/main/java/com/db/benchLib/configs/BenchLibClientsConfig.java
@@ -3,19 +3,33 @@ package com.db.benchLib.configs;
 import com.db.benchLib.clients.BenchesServiceClient;
 import com.db.benchLib.clients.RequestsServiceClient;
 import com.db.benchLib.utils.FeignSpringFormEncoder;
+import com.db.benchLib.utils.InMemoryMultipartFile;
 import feign.Feign;
 import feign.Logger;
-import feign.gson.GsonDecoder;
-import feign.gson.GsonEncoder;
+import feign.codec.Decoder;
 import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collection;
 
 @Configuration
 public class BenchLibClientsConfig {
+
+    private final ObjectFactory<HttpMessageConverters> messageConverters;
+
+    public BenchLibClientsConfig(ObjectFactory<HttpMessageConverters> messageConverters) {
+        this.messageConverters = messageConverters;
+    }
 
     @Bean
     @ConditionalOnProperty({"benchesService.host", "benchesService.port"})
@@ -25,7 +39,7 @@ public class BenchLibClientsConfig {
         return Feign.builder()
                 .client(new OkHttpClient())
                 .encoder(new FeignSpringFormEncoder())
-                .decoder(new GsonDecoder())
+                .decoder(decoder())
                 .logger(new Slf4jLogger(BenchesServiceClient.class))
                 .logLevel(Logger.Level.FULL)
                 .target(BenchesServiceClient.class, url);
@@ -39,10 +53,29 @@ public class BenchLibClientsConfig {
         return Feign.builder()
                 .client(new OkHttpClient())
                 .encoder(new FeignSpringFormEncoder())
-                .decoder(new GsonDecoder())
+                .decoder(decoder())
                 .logger(new Slf4jLogger(RequestsServiceClient.class))
                 .logLevel(Logger.Level.FULL)
                 .target(RequestsServiceClient.class, url);
     }
+
+    private Decoder decoder() {
+        Decoder decoder = (response, type) -> {
+            if (type instanceof Class && MultipartFile.class.isAssignableFrom((Class) type)) {
+                Collection<String> contentTypes = response.headers().get("content-type");
+                String contentType = "application/octet-stream";
+                if (contentTypes.size() > 0) {
+                    String[] temp = new String[contentTypes.size()];
+                    contentTypes.toArray(temp);
+                    contentType = temp[0];
+                }
+                byte[] bytes = StreamUtils.copyToByteArray(response.body().asInputStream());
+                return new InMemoryMultipartFile("file","", contentType,bytes);
+            }
+            return new SpringDecoder(messageConverters).decode(response, type);
+        };
+        return new ResponseEntityDecoder(decoder);
+    }
+
 
 }
