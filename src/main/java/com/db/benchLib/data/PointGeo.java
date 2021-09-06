122f0e6f588fcf261e45a08dd4e9d2ed7098aada
Asgar Zagitov <ihateyouthor@gmail.com>
2021-09-06 21:16:03 +0300

    0.0.32

--- src/main/java/com/db/benchLib/data/PointGeo.java
+++ src/main/java/com/db/benchLib/data/PointGeo.java
@@ -3,6 +3,7 @@ package com.db.benchLib.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.util.List;
@@ -10,7 +11,16 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
 public class PointGeo implements Serializable {
+
+    public static PointGeo point(List<Double> coordinates) {
+        return PointGeo.builder()
+                .type("Point")
+                .coordinates(coordinates)
+                .build();
+    }
+
     private String type;
     private List<Double> coordinates;
 }
