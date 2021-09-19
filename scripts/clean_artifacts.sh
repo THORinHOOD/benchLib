444bdc02220fd6f819f31d9431fa3a47979bbc4a
Asgar Zagitov <ihateyouthor@gmail.com>
2021-09-19 21:47:25 +0300

    grpc

--- scripts/clean_artifacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git checkout mvn-repo
-echo 'Start deleting extra artifacts...'
-rm -Rf -- ../target/mvn-repo/com/db/benchLib/
-git commit -m "deleted extra artifacts"
-git checkout master
-echo 'Done'
