ea36e46eecc6febc63ab03015100d9835a144a7c
Asgar Zagitov <ihateyouthor@gmail.com>
2021-09-19 20:05:12 +0300

    kek

--- scripts/clean_artifacts.sh
+++ scripts/clean_artifacts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git checkout mvn-repo
 echo 'Start deleting extra artifacts...'
-
+rm -Rf -- ../target/mvn-repo/com/db/benchLib/
+git commit -m "deleted extra artifacts"
 git checkout master
 echo 'Done'
